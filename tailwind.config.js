1902a7115333d6fbbd4c9a6c5702e347f4aed50c
TechisHeaven <vermajihimanshu2004@gmail.com>
2022-05-11 15:21:57 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   content: ["*"],
   theme: {
-    
+    screens: {
+      'sm-mx': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {
       fontFamily: {
         'popins': ['Poppins','sans-serif'],
